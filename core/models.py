35952cd2f42dd7705c837a1fc708a916c36d9911
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-20 19:28:57 -0300

    Ajuste

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#Aqui, ao que entendi, criamos os endpoints que queremos ter no site e, para cada um deles, criamos suas características
+
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField(default='')
-    aprovado = models.BooleanField()
+    aprovado = models.BooleanField(default=False)
 
 def __str__(self):
     return self.nome
