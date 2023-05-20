ae9c750d11a601759d6e50ca733e05547fc44db7
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-20 19:30:23 -0300

    Comentários

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#Aqui, pelo que entendi, registramos as API que criamos, para que elas façam parte do servidor que criamos.
 from .models import PontoTuristico
 
 admin.site.register(PontoTuristico)
